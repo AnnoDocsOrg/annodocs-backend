4966c628dba04f518ecf5d3f4445bbc405b8f964
AdrianBnghff <adrianbonninghoff@gmail.com>
2024-04-23 18:34:38 +0200

    Initial Commit

---
+++ src/main/java/com/annodocs/annodocsbackend/AnnodocsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.annodocs.annodocsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnodocsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnodocsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annodocs/annodocsbackend/AnnodocsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annodocs.annodocsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnodocsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
