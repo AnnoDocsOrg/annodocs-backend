2f79e203c7533e296bb947a03cd782d644c4a25f
AdrianBnghff <adrianbonninghoff@gmail.com>
2024-06-25 17:57:31 +0200

    Kleiner Fix

--- src/main/java/com/annodocs/annodocsbackend/JSONimport/controller/ImportController.java
+++ src/main/java/com/annodocs/annodocsbackend/JSONimport/controller/ImportController.java
@@ -2,19 +2,18 @@ package com.annodocs.annodocsbackend.JSONimport.controller;
 
 import com.annodocs.annodocsbackend.JSONimport.service.ImportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/api/gesetze")
 public class ImportController {
 
     @Autowired
     private ImportService importService;
 
-    @GetMapping("/import")
+    @PostMapping("/import")
     public String importData(@RequestParam String filePath) {
         try {
             importService.importData(filePath);
